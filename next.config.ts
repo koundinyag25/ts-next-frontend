f75c3ac7aaeb91ecdb100588f7856ac8bd800273
koundinyag25 <koundinya1991@gmail.com>
2025-01-23 02:52:27 +0530

    heroku deploy

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  distDir: "dist",
   /* config options here */
   env: {
-    NEXT_PUBLIC_API_BASE_URL: "http://localhost:5000",
+    NEXT_PUBLIC_API_BASE_URL: "https://ts-demo.moolya.xyz",
   },
 };
 
